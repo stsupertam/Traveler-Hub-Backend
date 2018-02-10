ca9634572d0c49772ee709e66de015928c793d93
stsupertam <supmantam@gmail.com>
2018-02-11 00:56:40 +0700

    remove unused module

--- config/express.js
+++ config/express.js
@@ -4,10 +4,6 @@ const compression = require('compression');
 const bodyParser = require('body-parser');
 const cors = require('cors')
 const passport = require('passport')
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const flash = require('connect-flash');
-
 
 module.exports = function() {
     var app = express()
@@ -23,9 +19,6 @@ module.exports = function() {
     app.use(bodyParser.json());
     app.use(passport.initialize())
     app.use(passport.session());
-    app.use(cookieParser('secret'));
-    app.use(session({cookie: { maxAge: 60000 }}));
-    app.use(flash());
     app.use(cors());
     require('../app/routes/user.route')(app);
     require('../app/routes/package.route')(app);
