f11ffb261522b2201c8de5f1fcce0121a82abc2b
stsupertam <supmantam@gmail.com>
2018-04-18 11:47:51 +0700

    fix birthdate bug for agency

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -9,7 +9,9 @@ exports.create = function(req, res, next) {
     user.validate()
         .then(() => {
             delete req.body.password
-            user.birthdate = new Date(req.body.birthdate)
+            if(req.body.birthdate) {
+                user.birthdate = new Date(req.body.birthdate)
+            }
             const token = jwt.sign(req.body, JWT_SECRET)
             user.save()
             return res.json({ message: 'Register Successfully', token: token })
